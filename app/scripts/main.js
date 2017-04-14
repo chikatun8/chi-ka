d18da1010b028246b67613cb5f2a58fb42b5dd3a
Luke Green <elukuent@gmail.com>
2017-04-13 21:43:53 -0700

    blocking out main sections, adding content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(document).ready(function(){
+
+  /*Change active class for menu items*/
+
+  $('.nav li').on('click', function(){
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  /*Fade in nav background*/
+
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > 100 ) {
+      $('.nav-bg-fade').fadeIn(500);
+    } else {
+      $('.nav-bg-fade').fadeOut(500);
+ 	  };
+  });
+
+  /*Skill box interactivity*/
+
+  $('.skill-box').hover(function(){
+    $(this).children('h2').fadeOut(500);
+
+  }, function(){
+    $(this).children('h2').fadeIn();
+  });
+
+})
